53a2f2a7334c84397c8de387916d31df7eeb32a9
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-06-07 15:38:45 +0200

    feat: front imgs added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Home page</h1>
-@endsection
+
+    <div class="container">
+        <h1>Home page </h1>
+        <div class="row">
+            <div class="col-6 col-md-4 col-lg-3 ">
+                @foreach ($products as $product)
+                <img src="{{Vite::asset('resources/img/' . $product['frontImage'])}}" alt="{{$product['name']}}">
+                    <p>{{$product['name']}}</p>
+                @endforeach             
+            </div>
+        </div>
+        </div>
+    @endsection
