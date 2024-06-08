ec80a521846c9134689c6cba48c2dc2c5ad5689e
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-06-08 09:40:27 +0200

    favorites change with db data, tags also change with db data

--- resources/views/partials/product-card.blade.php
+++ resources/views/partials/product-card.blade.php
@@ -2,13 +2,16 @@
     <img src="{{ Vite::asset('resources/img/' . $product['frontImage']) }}" class="img-fluid" alt="{{ $product['name'] }}">
     <div class="label">
         @foreach ($product['badges'] as $item)
-        {{-- @if ($item['type'] == "discount") --}}
-        <span>{{$item['value']}}</span>
-        {{-- <span>{{$item['value']}}</span> --}}
-        {{-- @endif --}}
+            @if ($item['type'] === 'tag')
+                <span @class(['green' => $item['type'] === 'tag'])>{{ $item['value'] }}</span>
+            @else
+                <span @class(['red' => $item['type'] === 'discount'])>{{ $item['value'] }}</span>
+            @endif
         @endforeach
     </div>
-    <div></div>
+    <div class="heart">
+        <span @class(['orange' => $product['isInFavorites']])>&hearts;</span>
+    </div>
 </div>
 <div>
     <p>{{ $product['brand'] }}</p>
